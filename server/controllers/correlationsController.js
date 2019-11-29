5b751e3f04c3cdf031b053ef14d83db669ac1ec9
Jere Helén <jere.helen@helsinki.fi>
2019-11-29 19:15:05 +0200

    Add mankeli

--- server/controllers/correlationsController.js
+++ server/controllers/correlationsController.js
@@ -1,7 +1,24 @@
 const correlations = require('@root/data/correlations.json')
 
+const mankeli = ({ courses }) => {
+  const keys = Object.keys(courses)
+  return keys.reduce((acc, k) => {
+    console.log(courses[k])
+    const { correlations } = courses[k]
+    const corrkeys = Object.keys(correlations)
+    var acualcorrs = corrkeys.reduce((corracc, corrk) => {
+      return corracc.concat({ name: corrk, correlation: correlations[corrk] })
+    }, [])
+
+    return acc.concat({
+      name: k,
+      correlations: acualcorrs.filter((c) => c.correlation > 0.2)
+    })
+  }, [])
+}
+
 const getAll = async (req, res) => {
-  res.send(correlations)
+  res.send(mankeli(correlations))
 }
 
 module.exports = {
